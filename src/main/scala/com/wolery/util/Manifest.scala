fd1f7641160880d80fa3cafffb644474ea0088d4
Jonathon Bell <jonathon.a.bell@gmail.com>
2018-02-22 19:24:41 -0700

    Add function `println`
    Also: lift regular expression out of `format` to avoid redundant
    recompilation.

--- src/main/scala/com/wolery/util/Manifest.scala
+++ src/main/scala/com/wolery/util/Manifest.scala
@@ -16,6 +16,7 @@ package com.wolery
 package util
 
 import java.util.jar.Manifest
+import scala.util.matching.Regex
 
 /**
  * Miscellaneous utility functions for reading the manifest.
@@ -44,12 +45,12 @@ trait manifest
   }
 
   /**
-   * Replace the occurrences of attribute placeholders within the given string
-   * with their respective values in the manifest.
+   * Replaces occurrences of attribute  placeholders within the given template
+   * string with their respective values in the manifest.
    *
    * An attribute placeholder is specified with the syntax `${<attribute>}` as
    * if referring to a variable from within a Scala string interpolation.  The
-   * attribute values themselves are specified in the parent POM  and embedded
+   * attribute values themselves are specified in the parent POM, and embedded
    * within the manifest at build time.
    *
    * For example:
@@ -63,17 +64,35 @@ trait manifest
    *
    * @param  string  A template string containing attribute placeholders.
    *
-   * @return  A copy of the input string in which each plaheholder is replaced
-   *          its respective value in the manifest, or the empty string if no
-   *          such attribute exists.
+   * @return A copy of the input string in  which each placeholder is replaced
+   *         with its respective value in the manifest, or the empty string if
+   *         no such attribute exists.
    */
   def format(string: String): String =
   {
     val a = attributes                                   // Get the attributes
-    val p = """\$\{([A-Za-z-]+)\}""".r                   // Match "${<ident>}"
+    val p = m_regex                                      // Matches ${<ident>}
 
     p.replaceAllIn(string,m ⇒ a.getOrElse(m.group(1),""))// Replace with value
   }
+
+  /**
+   * Replaces occurrences of attribute  placeholders within the given template
+   * string with their respective values in the manifest and prints the result
+   * to the console.
+   *
+   * @param  string  A template string containing attribute placeholders.
+   */
+  def println(string: String): Unit =
+  {
+    Console.println(format(string))                      // Format, then print
+  }
+
+  /**
+   * A regular expression that matches an attribute placeholder.
+   */
+  private lazy
+  val m_regex: Regex = """\$\{([A-Za-z-]+)\}""".r        // Matches ${<ident>}
 }
 
 /**
