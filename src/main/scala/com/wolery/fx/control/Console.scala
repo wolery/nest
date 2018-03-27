2bcafc34a8b434f6ef8e4baca20a502141e5df76
Jonathon Bell <jonathon.a.bell@gmail.com>
2018-03-27 05:46:08 -0400

    Clear existing style class names.

--- src/main/scala/com/wolery/fx/control/Console.scala
+++ src/main/scala/com/wolery/fx/control/Console.scala
@@ -140,7 +140,8 @@ class Console extends TextArea with Logging
                                        (KEY_TYPED,  onKeyTyped   _))
 
   swap(Seq(),m_filters)                                  // Initialize filters
-  getStyleClass.add("console");                          // Style class name
+  getStyleClass.clear()                                  // Clear class styles
+  getStyleClass.add("console")                           // Style class name
 
   /**
    * A bean property that records the current event handler for the `Accept`
